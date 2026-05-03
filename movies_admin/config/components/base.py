b08e3804b08aebf60a724dcb6b428a06ae2767ba
Anton Zlobin <mailforpy62183@yandex.ru>
2026-05-03 07:12:41 +0000

    Убрать .env и добавить .env.example без чувствительной инфо

--- movies_admin/config/components/base.py
+++ movies_admin/config/components/base.py
@@ -3,4 +3,4 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
